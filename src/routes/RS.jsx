a7d9011d5d7fc121ff0c2fb135f898392c775d86
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-04-28 13:58:12 -0300

    finish

--- src/routes/RS.jsx
+++ src/routes/RS.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Imgs from "../components/Imgs";
+import Text from "../components/Text";
+import { useTranslation } from "react-i18next";
+import './routes.css'
 
 const RS = () => {
-    return ( <h1>rs</h1> );
+    const {t} = useTranslation();
+    return ( 
+        <>
+        <h1>Rio Grande do Sul</h1>
+        <div className="containercity">
+            <Imgs
+            url = '/img/rs-1.jpg'
+            name = 'Porto Alegre'
+            />
+            <Text
+            text={t('rs-1')}
+            />
+        </div>
+
+        <div className="containercity">
+            
+            <Text
+            text={t('rs-2')}
+            />
+            <Imgs
+            url = '/img/rs-canela.jpg'
+            name = 'Gramado e Canela'
+            />
+        </div>
+
+        <div className="containercity">
+            <Imgs
+            url = '/img/rs-garibaldi.jpg'
+            name = 'Garibaldi'
+            />
+            <Text
+            text={t('rs-3')}
+            />
+        </div>
+    </> 
+    );
 }
  
 export default RS;
--- src/routes/TO.jsx
+++ src/routes/TO.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Imgs from "../components/Imgs";
+import Text from "../components/Text";
+import { useTranslation } from "react-i18next";
+import './routes.css'
 
 const TO = () => {
-    return ( <h1>to</h1> );
+    const {t} = useTranslation();
+    return ( 
+        <>
+        <h1>Tocantins</h1>
+        <div className="containercity">
+            <Imgs
+            url = '/img/to-aurora.jpg'
+            name = 'Aurora'
+            />
+            <Text
+            text={t('to-1')}
+            />
+        </div>
+
+        <div className="containercity">
+            
+            <Text
+            text={t('to-2')}
+            />
+            <Imgs
+            url = '/img/to-jalapao.jpg'
+            name = 'Jalapão'
+            />
+        </div>
+
+        <div className="containercity">
+            <Imgs
+            url = '/img/to-lajeando.jpg'
+            name = 'Lajeando'
+            />
+            <Text
+            text={t('to-3')}
+            />
+        </div>
+    </> 
+    );
 }
  
 export default TO;
